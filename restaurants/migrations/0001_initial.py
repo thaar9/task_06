2aa8899fdab9bedc150513be68a020cf2613cfce
thaar9 <34986623+thaar9@users.noreply.github.com>
2019-11-18 07:22:30 +0100

    Update 0001_initial.py

--- restaurants/migrations/0001_initial.py
+++ restaurants/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Restaurant',
+            name='Das Restaurant',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=120)),
